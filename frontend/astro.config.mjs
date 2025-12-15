22fe61e411a062e2b47e65e7f511115240391b5d
Sofia Santos <sofiarsantos31@gmail.com>
2025-12-15 12:27:55 +0000

    fix graphs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  //output: 'server',
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
